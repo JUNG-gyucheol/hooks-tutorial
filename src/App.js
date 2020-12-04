2c9f2ea60ca4e28f68cbf3d34f7a6c8aeeccc0ac
ggyu <kcpooh0202@naver.com>
2020-12-04 13:36:10 +0900

    react-hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React, { useState } from "react";
 import "./App.css";
+import Counter from "./Counter";
 import Info from "./Info";
+import Average from "./Average";
 function App() {
   return (
     <div className="App">
-      <Info />
+      <Average />
     </div>
   );
 }
--- src/Info.js
+++ src/Info.js
@@ -1,34 +1,49 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
+import useInputs from "./useInputs";
 
 function Info() {
-  const [name, setName] = useState("");
-  const [nickname, setNickname] = useState("");
+  const [state, onChange] = useInputs({
+    name: "",
+    nickname: "",
+  });
+  const { name, nickname } = state;
 
   //마운트와 업데이트 둘다 될 때
-  useEffect(() => {
-    console.log("렌더링 완료되었습니다.(마운트와 업데이트 될 때 실행)");
-    console.log({
-      name,
-      nickname,
-    });
-  });
+  //   useEffect(() => {
+  //     console.log("렌더링 완료되었습니다.(마운트와 업데이트 될 때 실행)");
+  //     console.log({
+  //       name,
+  //       nickname,
+  //     });
+  //   });
   //마운트만 될 때
-  useEffect(() => {
-    console.log("마운트 될 때만 실행됩니다.");
-  }, []);
+  //   useEffect(() => {
+  //     console.log("마운트 될 때만 실행됩니다.");
+  //   }, []);
+  // 업데이트 될 때 실행
+  //   useEffect(() => {
+  //     console.log("업데이트 될 때만 실행됩니다.");
+  //     console.log(name);
+  //   }, [name]);
+  //뒷정리함수
+  //   useEffect(() => {
+  //     console.log("effect");
+  //     console.log(name);
+  //     return () => {
+  //       console.log("cleanup");
+  //       console.log(name);
+  //     };
+  //   }, [name]);
 
-  const onNameChange = (e) => {
-    setName(e.currentTarget.value);
-  };
-  const onNicknameChange = (e) => {
-    setNickname(e.currentTarget.value);
-  };
+  //   const onNicknameChange = (e) => {
+  //     setNickname(e.currentTarget.value);
+  //   };
   console.log("render");
   return (
     <div>
       <div>
-        <input value={name} onChange={onNameChange} />
-        <input value={nickname} onChange={onNicknameChange} />
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <b>이름 : {name}</b>
